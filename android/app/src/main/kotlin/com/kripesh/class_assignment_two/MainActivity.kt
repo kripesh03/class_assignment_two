12e6ed4a047f0359721b6cd4dea84e2b54fc8b56
kripesh03 <kripeshpoudel9842@gmail.com>
2024-12-27 12:16:26 +0545

    empty project

---
+++ android/app/src/main/kotlin/com/kripesh/class_assignment_two/MainActivity.kt
@@ -0,0 +1,5 @@
+package com.kripesh.class_assignment_two
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity()
---
+++ lib/main.dart
@@ -0,0 +1,8 @@
+import 'package:class_assignment_two/app.dart';
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(
+    const App(),
+  );
+}
